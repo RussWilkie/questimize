f4b46a3768f6ae83d9f6e222104d908f26964d71
RussWilkie <russwilkie@gmail.com>
2021-10-18 18:04:33 -0500

    updated quests vue functionality

---
+++ app/Http/Controllers/QuestController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Quest;
+use Illuminate\Http\Request;
+
+class QuestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Quest::latest()->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,
+        [
+            'title' => 'required'
+        ],
+        [
+            'title.required' => 'Quest input field is required!'
+        ]
+        );
+        Quest::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Quest  $quest
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Quest $quest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Quest  $quest
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Quest $quest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Quest  $quest
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+        $quest = Quest::findOrFail($id);
+        $quest->update($request->all());
+        $quest->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Quest  $quest
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $quest = Quest::findOrFail($id);
+        $quest->delete();
+        return Quest::latest()->get();
+    }
+}
